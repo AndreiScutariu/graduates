0325479378be111149a9fb74396b00bcfefed05f
ivmartie <ivmartie@yahoo.com>
2016-04-05 20:56:39 +0300

    update

---
+++ SingleResponsability/Good/UserInfo.cs
@@ -0,0 +1,26 @@
+﻿namespace SingleResponsability.Good
+{
+    public class UserInfo
+    {
+
+        private User _user;
+
+        public UserInfo(User user)
+        {
+            _user = user;
+        }
+
+        public bool IsValid()
+        {
+            bool isValid = false;
+            return isValid;
+        }
+
+
+        public int GetAge()
+        {
+            int age = 0;
+            return age;
+        }
+    }
+}
