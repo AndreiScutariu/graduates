d00f1807ea83fd64242c990d8127f3f598e54369
ivmartie <ivmartie@yahoo.com>
2016-04-05 20:59:40 +0300

    update 2

---
+++ SingleResponsability/Good/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SingleResponsability.Good
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime BirthDay { get; set; }
+
+        public Guid UserId { get; set; }
+    }
+}
