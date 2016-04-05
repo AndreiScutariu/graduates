3e527cfd80855af404d8f2cc124c4c80277c0f4c
Andrei Scutariu <Andrei.Scutariu@endava.com>
2016-04-05 21:03:54 +0300

    commit 2

---
+++ OpenClosed/Good/Logger.cs
@@ -0,0 +1,45 @@
+﻿namespace OpenClosed.Good
+{
+    public class Logger
+    {
+        private readonly TypeLog _typelog;
+
+        public Logger(TypeLog typelog)
+        {
+            _typelog = typelog;
+        }
+
+        public void WriteLog(string message)
+        {
+            switch (_typelog)
+            {
+                case TypeLog.File:
+                    WriteInFile(message);
+                    break;
+                case TypeLog.DataBase:
+                    WriteInDataBase(message);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void WriteInFile(string message)
+        {
+            //Do stuff
+        }
+
+        private void WriteInDataBase(string message)
+        {
+            //Do stuff
+        }
+
+    }
+
+    public enum TypeLog
+    {
+        File,
+        DataBase
+    }
+
+}
