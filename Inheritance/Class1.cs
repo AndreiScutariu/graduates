2f9196dfee3cb8050c5cb00d74435141f0b51c88
ivmartie <ivmartie@yahoo.com>
2016-04-05 21:35:18 +0300

    inheritance

--- Inheritance/Class1.cs
+++ Inheritance/Class1.cs
@@ -1,12 +1,70 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Inheritance
 {
-    public class Class1
+    internal abstract class BasePlayer
     {
+        public abstract string Manufacturer { get; }
+
+        public string FileName { get; }
+
+        public abstract void Play();
+
+        public abstract string Skip(int seconds);
     }
+
+    abstract class Mp3Player:BasePlayer
+    {
+        
+        public override void Play()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string Skip(int seconds)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    abstract class VideoPlayer : BasePlayer
+    {
+
+        public override void Play()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string Skip(int seconds)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class SonyMp3Player : Mp3Player
+    {
+        public override string Manufacturer {
+            get
+            {
+                return "Sony";
+            }
+        }
+    }
+
+    internal class PlayVideoComponent
+    {
+        public void Play()
+        {
+        }
+    }
+
+    internal class SkipVideoComponent
+    {
+        public void Skip()
+        {
+        }
+    }
+
+
+
 }
